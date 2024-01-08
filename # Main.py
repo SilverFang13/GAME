6b0a5ff8d4facc058ed0df3a153d94933bb2e862
Michaeldrst <126831551+Michaeldrst@users.noreply.github.com>
2024-01-08 10:11:43 -0600

    Michael only work on this part
    I DID ALL THE WORK ON THIS PART NO ONE WANTED TO HELP

---
+++ # Main.py
@@ -0,0 +1,11 @@
+# Main
+
+#Imports pygame
+import pygame
+pygame.init()
+
+#Temp Display would like to have a display so that it fits full screen by detecting monitor size and ajusting size upon the detection.
+displayWidth = 800
+displayHeight = 600
+surface = pygame.display
+
---
+++ # Player Momvent Commands.py
@@ -0,0 +1,29 @@
+# Player Momvent Commands
+
+# Will move the player forward a particualr ammount of Pixles
+from turtle import onkeypress
+
+
+def ForwardMovement():
+    onkeypress('w')
+
+
+# Will move the player Left a particualr ammount of Pixles
+
+
+def LeftMovement():
+    onkeypress('a') 
+
+
+# Will move the player Right a particualr ammount of Pixles
+
+
+def RightMovement():
+    onkeypress('d') 
+
+
+# Will move the player Backward a particualr ammount of Pixles
+
+
+def BackwardMovement():
+    onkeypress('s')  
